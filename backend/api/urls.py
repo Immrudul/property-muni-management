162361592e5b7d56b9df1da20dde6df6c33d9b8b
Mrudul Suresh <mail2mrudul@gmail.com>
2025-03-16 13:38:54 -0400

    start new property_assessment app

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-
+from django.contrib import admin
+from django.urls import path, include
+from api.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    
-]
+    path("user/register/", CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("api-auth/", include("rest_framework.urls")),
+
+]   
