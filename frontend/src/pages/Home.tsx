5e46f2e8faf0335c4d761efdea3137b8882b68ec
Mrudul Suresh <mail2mrudul@gmail.com>
2025-03-17 16:22:04 -0400

    tailwindcss intallation + setup

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import "../index.css"
 
 const Home = () => {
   const navigate = useNavigate();
